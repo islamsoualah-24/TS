b9856cb3f012e78effb937ec7b2b110bd0f6ba25
islamsoualah-24 <islamsoualah2017@gmail.com>
2024-06-03 16:46:47 +0100

    Update data_loader.py

--- tsf-new-paper-taste-master/data_provider/data_loader.py
+++ tsf-new-paper-taste-master/data_provider/data_loader.py
@@ -11,92 +11,108 @@ import warnings
 warnings.filterwarnings('ignore')
 
 class Weather_Dataset(Dataset):
-    
-    def __init__(self, root_path, flag='train', size=None,
-                 features='S', data_path='ETTh1.csv',
-                 target='OT', scale=True, timeenc=0, freq='h'):
-        # size [seq_len, label_len, pred_len]
-        # info
-        if size == None:
-            self.seq_len = 24 * 4 * 4
-            self.label_len = 24 * 4
-            self.pred_len = 24 * 4
-        else:
-            self.seq_len = size[0]
-            self.label_len = size[1]
-            self.pred_len = size[2]
-        # init
-        assert flag in ['train', 'test', 'val']
-        type_map = {'train': 0, 'val': 1, 'test': 2}
-        self.set_type = type_map[flag]
+    def __init__(
+        self, data, batch_size, seq_len, pred_len, feature_type, target='OT'
+    ):
+      self.data = data
+      self.batch_size = batch_size
+      self.seq_len = seq_len
+      self.pred_len = pred_len
+      self.feature_type = feature_type
+      self.target = target
+      self.target_slice = slice(0, None)
 
-        self.features = features
-        self.target = target
-        self.scale = scale
-        self.timeenc = timeenc
-        self.freq = freq
-        self.root_path = root_path
-        self.data_path = data_path
-        self.__read_data__()
+      self._read_data()
 
-    def __read_data__(self):
-        self.scaler = StandardScaler()
-        df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
+    def _read_data(self):
+    """Load raw data and split datasets."""
 
-        border1s = [0, 12 * 30 * 24 - self.seq_len, 12 * 30 * 24 + 4 * 30 * 24 - self.seq_len]
-        border2s = [12 * 30 * 24, 12 * 30 * 24 + 4 * 30 * 24, 12 * 30 * 24 + 8 * 30 * 24]
-        border1 = border1s[self.set_type]
-        border2 = border2s[self.set_type]
+    # copy data from cloud storage if not exists
+      if not os.path.isdir(LOCAL_CACHE_DIR):
+        os.mkdir(LOCAL_CACHE_DIR)
 
-        if self.features == 'M' or self.features == 'MS':
-            cols_data = df_raw.columns[1:]
-            df_data = df_raw[cols_data]
-        elif self.features == 'S':
-            df_data = df_raw[[self.target]]
+      file_name = self.data + '.csv'
+      cache_filepath = os.path.join(LOCAL_CACHE_DIR, file_name)
+      if not os.path.isfile(cache_filepath):
+        tf.io.gfile.copy(
+            os.path.join(DATA_DIR, file_name), cache_filepath, overwrite=True
+        )
 
-        if self.scale:
-            train_data = df_data[border1s[0]:border2s[0]]
-            self.scaler.fit(train_data.values)
-            data = self.scaler.transform(df_data.values)
-        else:
-            data = df_data.values
+      df_raw = pd.read_csv(cache_filepath)
 
-        df_stamp = df_raw[['date']][border1:border2]
-        df_stamp['date'] = pd.to_datetime(df_stamp.date)
-        if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            data_stamp = df_stamp.drop(['date'], 1).values
-        elif self.timeenc == 1:
-            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
-            data_stamp = data_stamp.transpose(1, 0)
+    # S: univariate-univariate, M: multivariate-multivariate, MS:
+    # multivariate-univariate
+      df = df_raw.set_index('date')
+      if self.feature_type == 'S':
+        df = df[[self.target]]
+      elif self.feature_type == 'MS':
+        target_idx = df.columns.get_loc(self.target)
+        self.target_slice = slice(target_idx, target_idx + 1)
 
-        self.data_x = data[border1:border2]
-        self.data_y = data[border1:border2]
-        self.data_stamp = data_stamp
+    # split train/valid/test
+      n = len(df)
+      if self.data.startswith('ETTm'):
+        train_end = 12 * 30 * 24 * 4
+        val_end = train_end + 4 * 30 * 24 * 4
+        test_end = val_end + 4 * 30 * 24 * 4
+      elif self.data.startswith('ETTh'):
+        train_end = 12 * 30 * 24
+        val_end = train_end + 4 * 30 * 24
+        test_end = val_end + 4 * 30 * 24
+      else:
+        train_end = int(n * 0.7)
+        val_end = n - int(n * 0.2)
+        test_end = n
+      train_df = df[:train_end]
+      val_df = df[train_end - self.seq_len : val_end]
+      test_df = df[val_end - self.seq_len : test_end]
 
-    def __getitem__(self, index):
-        s_begin = index
-        s_end = s_begin + self.seq_len
-        r_begin = s_end - self.label_len
-        r_end = r_begin + self.label_len + self.pred_len
+    # standardize by training set
+      self.scaler = StandardScaler()
+      self.scaler.fit(train_df.values)
 
-        seq_x = self.data_x[s_begin:s_end]
-        seq_y = self.data_y[r_begin:r_end]
-        seq_x_mark = self.data_stamp[s_begin:s_end]
-        seq_y_mark = self.data_stamp[r_begin:r_end]
+      def scale_df(df, scaler):
+        data = scaler.transform(df.values)
+        return pd.DataFrame(data, index=df.index, columns=df.columns)
 
-        return seq_x, seq_y, seq_x_mark, seq_y_mark
+      self.train_df = scale_df(train_df, self.scaler)
+      self.val_df = scale_df(val_df, self.scaler)
+      self.test_df = scale_df(test_df, self.scaler)
+      self.n_feature = self.train_df.shape[-1]
 
-    def __len__(self):
-        return len(self.data_x) - self.seq_len - self.pred_len + 1
+    def _split_window(self, data):
+      inputs = data[:, : self.seq_len, :]
+      labels = data[:, self.seq_len :, self.target_slice]
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+      inputs.set_shape([None, self.seq_len, None])
+      labels.set_shape([None, self.pred_len, None])
+      return inputs, labels
+
+    def _make_dataset(self, data, shuffle=True):
+      data = np.array(data, dtype=np.float32)
+      ds = tf.keras.utils.timeseries_dataset_from_array(
+          data=data,
+          targets=None,
+          sequence_length=(self.seq_len + self.pred_len),
+          sequence_stride=1,
+          shuffle=shuffle,
+          batch_size=self.batch_size,
+      )
+      ds = ds.map(self._split_window)
+      return ds
 
     def inverse_transform(self, data):
-        return self.scaler.inverse_transform(data)
+      return self.scaler.inverse_transform(data)
 
+    def get_train(self, shuffle=True):
+      return self._make_dataset(self.train_df, shuffle=shuffle)
+
+    def get_val(self):
+      return self._make_dataset(self.val_df, shuffle=False)
+
+    def get_test(self):
+      return self._make_dataset(self.test_df, shuffle=False)
 
 class Dataset_ETT_hour(Dataset):
     def __init__(self, root_path, flag='train', size=None,
