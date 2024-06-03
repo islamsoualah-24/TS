729312b29aa2ff001de730b8c37edfab1a4cbc26
islamsoualah-24 <islamsoualah2017@gmail.com>
2024-06-03 17:57:33 +0100

    Update data_loader.py

--- tsf-new-paper-taste-master/data_provider/data_loader.py
+++ tsf-new-paper-taste-master/data_provider/data_loader.py
@@ -190,7 +190,7 @@ class Dataset_ETT_minute(Dataset):
 
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None,
-                 features='S', data_path='ETTh1.csv',
+                 features='S', data_path,
                  target='OT', scale=True, timeenc=0, freq='h'):
         # size [seq_len, label_len, pred_len]
         # info
