3e83cead1e724c95e8dda8e087ff1844625823dd
islamsoualah-24 <islamsoualah2017@gmail.com>
2024-06-05 16:58:35 +0100

    Update TSMixer.py

--- tsf-new-paper-taste-master/models/TSMixer.py
+++ tsf-new-paper-taste-master/models/TSMixer.py
@@ -135,7 +135,7 @@ class Backbone(nn.Module):
         self.mix_layer = Mixer_Layer(seq_len, enc_in)
         self.temp_proj = nn.Linear(self.seq_len, self.pred_len)
         # Define a convolutional layer
-        self.conv_layer = nn.Conv1d(in_channels=self.enc_in, out_channels=self.enc_in, kernel_size=3, padding=1)
+        #self.conv_layer = nn.Conv1d(in_channels=self.enc_in, out_channels=self.enc_in, kernel_size=3, padding=1)
     def forward(self, x): # B, L, D -> B, H, D
         # Apply convolutional layer
         #x = x.permute(0, 2, 1)  # B, L, D -> B, D, L
